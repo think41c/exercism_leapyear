2f63d82f91707ef322ec462f0b732e4489191cbb
Tony DiNitto <think41c@gmail.com>
2015-03-29 22:07:38 -0600

    Logic for 100, 400, 4 years implemented.

--- year.rb
+++ year.rb
@@ -1,7 +1,12 @@
 class Year
 
   def self.leap?(year)
-    'Yes, 1996 is a leap year'
+
+    if (year % 400 == 0) || (year % 4 == 0 ) && (year % 100 != 0) 
+    'Yes, #{year} is a leap year'
+    else
+    "No, #{year} is not a leap year"
+    end
   end
 
 end
